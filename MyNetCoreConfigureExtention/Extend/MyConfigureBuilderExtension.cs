e699a041eff6803a5c60b07742b6f504bb04b7da
zhangy04 <zhangy04@mingyuanyun.com>
2021-05-26 17:59:07 +0800

    配置选项

---
+++ MyNetCoreConfigureExtention/Extend/MyConfigureBuilderExtension.cs
@@ -0,0 +1,17 @@
+﻿using MyNetCoreConfigureExtention;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Microsoft.Extensions.Configuration
+{
+    public static class MyConfigureBuilderExtension
+    {
+        public static IConfigurationBuilder AddMyConfigure(this IConfigurationBuilder builder)
+        {
+            builder.Add(new MyConfigureSource());
+            return builder;
+        }
+    }
+}
